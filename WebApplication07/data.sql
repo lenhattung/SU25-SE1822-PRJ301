734b7a2ee7147b20834f0b4e1e34d1cf686190ea
Tung Le <lenhattung@gmail.com>
2025-06-03 10:43:56 +0700

    ud
    ud

--- WebApplication07/data.sql
+++ WebApplication07/data.sql
@@ -29,3 +29,29 @@ INSERT [dbo].[tblUsers] ([userID], [fullName], [password], [roleID], [status]) V
 INSERT [dbo].[tblUsers] ([userID], [fullName], [password], [roleID], [status]) VALUES (N'qe175533', N'Pham Tuan Minh', N'qn', N'MB', 1)
 INSERT [dbo].[tblUsers] ([userID], [fullName], [password], [roleID], [status]) VALUES (N'ce175533', N'Mai Thanh Quan', N'ct', N'MB', 1)
 go
+
+CREATE TABLE [dbo].[tblProducts](
+	[id] [char](5) NOT NULL,
+	[name] [nvarchar](50) NOT NULL,
+	[description] [nvarchar](500) NOT NULL,
+	[price] [float] NOT NULL,
+	[size] [nvarchar](50) NOT NULL,
+	[status] [bit] NOT NULL,
+ CONSTRAINT [PK_tblProducts] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+SET ANSI_PADDING OFF
+GO
+INSERT [dbo].[tblProducts] ([id], [name], [description], [price], [size], [status]) VALUES (N'F-001', N'Fashion dresses', N'Fashion dresses for Summer 2025', 285,'L', 1)
+INSERT [dbo].[tblProducts] ([id], [name], [description], [price], [size], [status]) VALUES (N'F-002', N'Men''s shirt', N'VIET TIEN long-sleeve men''s shirt', 115,'M', 1)
+INSERT [dbo].[tblProducts] ([id], [name], [description], [price], [size], [status]) VALUES (N'F-003', N'Short office skirt', N'Short skirt for women''s office wear', 307,'S', 1)
+INSERT [dbo].[tblProducts] ([id], [name], [description], [price], [size], [status]) VALUES (N'F-004', N'Vietnamese Ao Dai', N'Traditional Vietnamese Ao Dai dress', 436,'M', 1)
+INSERT [dbo].[tblProducts] ([id], [name], [description], [price], [size], [status]) VALUES (N'F-005', N'Women''s shirt', N'AN PHUOC Ladies'' White Long-Sleeve Shirt', 155,'S', 0)
+INSERT [dbo].[tblProducts] ([id], [name], [description], [price], [size], [status]) VALUES (N'F-006', N'Men''s jeans', N'Men''s jeans - LEVIS', 327,'L', 1)
+INSERT [dbo].[tblProducts] ([id], [name], [description], [price], [size], [status]) VALUES (N'F-007', N'Women''s trousers', N'An Phuoc Ladies'' Plain Dark Blue Trousers', 235,'M', 1)
+INSERT [dbo].[tblProducts] ([id], [name], [description], [price], [size], [status]) VALUES (N'F-008', N'Men''s blazer', N'Men''s suit jacket', 753,'L', 0)
+INSERT [dbo].[tblProducts] ([id], [name], [description], [price], [size], [status]) VALUES (N'F-009', N'Ladies'' t-shirt', N'Ladies'' activewear t-shirt', 270,'S', 1)
+GO
